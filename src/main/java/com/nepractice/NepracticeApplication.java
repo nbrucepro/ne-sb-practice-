2507a21fa24b9003fe7b4fe608221be4716a1e8c
nbrucepro <nezabruce@gmail.com>
2023-06-12 17:16:52 +0200

    first commit

---
+++ src/main/java/com/nepractice/NepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.nepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepracticeApplication.class, args);
+	}
+
+}
