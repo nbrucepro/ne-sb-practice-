c60dab0f78f4287573279910915107119a6bad33
nbrucepro <nezabruce@gmail.com>
2023-06-14 19:00:26 +0200

    logout-service

--- src/main/java/com/nepractice/NepracticeApplication.java
+++ src/main/java/com/nepractice/NepracticeApplication.java
@@ -2,13 +2,19 @@ package com.nepractice;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @RestController
 @RequestMapping("api/v1/customers")
 public class NepracticeApplication {
