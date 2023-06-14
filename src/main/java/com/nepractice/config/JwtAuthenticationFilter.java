c8cda4349eb4a41e4db9b61f6b795ae4e68eac7b
nbrucepro <nezabruce@gmail.com>
2023-06-14 12:19:13 +0200

    jwt-service stuffs

--- src/main/java/com/nepractice/config/JwtAuthenticationFilter.java
+++ src/main/java/com/nepractice/config/JwtAuthenticationFilter.java
@@ -1,4 +1,18 @@
 package com.nepractice.config;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
